46f080e2390a1eb13ea7d973522e1a936e90ef0f
yayoi03 <uni.ani367@gmail.com>
2020-04-15 02:16:32 +0900

    add add lightspeed.go

---
+++ lesson2/lightspeed.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var(
+	// 	const lightspeed = 299792
+	// var distance = 56000000
+	// )
+	//var distance,speed=56000000,100800
+	const lightspeed = 299792
+	var distance = 56000000
+	fmt.Println(distance/lightspeed, "秒")
+}
